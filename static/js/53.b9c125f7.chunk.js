4b96579c79fde4604199abaeb3e12a0e992fa6c8
NewUsername1234 <ilja.romanov@yahoo.com>
2021-08-04 00:19:03 +0200

    pv5

---
+++ static/js/53.b9c125f7.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpionic-app"]=this["webpackJsonpionic-app"]||[]).push([[53],{137:function(n,t,i){"use strict";i.r(t),i.d(t,"startStatusTap",(function(){return c}));var e=i(13),o=i(12),c=function(){var n=window;n.addEventListener("statusTap",(function(){Object(e.h)((function(){var t=n.innerWidth,i=n.innerHeight,c=document.elementFromPoint(t/2,i/2);if(c){var r=c.closest("ion-content");r&&new Promise((function(n){return Object(o.c)(r,n)})).then((function(){Object(e.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.b9c125f7.chunk.js.map
